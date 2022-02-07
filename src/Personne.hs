9509aa6ab843e5352ed8093bdd4114371f98edc7
cryptoswarm <mokhtar.safir@outlook.com>
2022-02-06 23:51:34 -0500

    Validate email

--- src/Personne.hs
+++ src/Personne.hs
@@ -1,12 +1,19 @@
-module Personne where 
+--import Data.Foldable
+--import Data.List
+
+module Personne where
+
+import Data.Maybe (fromJust)
+import Data.List (elemIndex)
+import Data.Char
 
 type Nom = String
 type Prenom = String
 type Courriel = String
 type Signature = (Prenom, Nom)
-data Personne = Personne 
-                Courriel 
-                Signature 
+data Personne = Personne
+                Courriel
+                Signature
                 deriving (Show, Read) -- Courriel unique
 instance Eq Personne where
     (==) (Personne cour1 _ ) (Personne cour2 _ ) = cour1 == cour2
@@ -28,4 +35,41 @@ courriel (Personne c _)= c
 -- >>> map courrielValide ["tatoooange@smail.ca", "ange.tato@smail.ca", "ange_tato@smail.ca", "Tato@smail.ca"]
 -- [True,True,True,False]
 courrielValide :: [Char] -> Bool
-courrielValide = error "à acompléter"
+--courrielValide = error "à acompléter"
+-- courrielValide xs = if not '@'  `elem` xs then return False
+courrielValide xs
+                | '@' `notElem`  xs = False
+                | domain /= "@smail.ca" = False
+                | otherwise = True
+                where (name, domain) = splitByIndex '@' xs
+
+
+
+getIndexOf:: Eq a => a -> [a] -> Int
+getIndexOf x xs = fromJust $ elemIndex x xs
+
+splitByIndex:: Eq a =>  a -> [a]-> ([a], [a])
+splitByIndex x xs = splitAt (getIndexOf x xs) xs
+
+-- takes a name and a list of allowed chars, 
+-- Compare each char of name to allowed chars
+--heckName:: Eq a => [a] -> [a]-> Bool
+checkName [] = False
+checkName (x:xs1) 
+    | elem x numbers = True 
+    | elem x caracters && isLower x = True 
+    | otherwise = False 
+    where numbers = getDigits xs1
+          caracters = getCaracters xs1
+
+
+getDigits :: [Char] -> [Char ]
+getDigits [] = []
+getDigits xs = filter isDigit xs
+
+getCaracters:: [Char ] -> [Char ]
+getCaracters [] = []
+getCaracters (x:xs)
+    | isDigit x =  getCaracters xs
+    | otherwise = x : getCaracters xs
+
